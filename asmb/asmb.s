eadd2e9a93f4dff2eb80b2c91d4e30915db42aee
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-03-07 21:16:26 +0100

    comments added

--- asmb/asmb.s
+++ asmb/asmb.s
@@ -6,11 +6,11 @@ asmb:
 .LFB0:
 	.cfi_startproc
 	mov %rsi, %rcx		# start our counter with length of array
-	cmp $0, %rcx
+	cmp $0, %rcx			# compare once so zero flag 
 start:
-	je end
-	dec %rcx
-	rcrq $1, (%rdi, %rcx, 8)
+	jz end 						# if %rcx is 0, we have reached the end of the array
+	dec %rcx					# this will set the zero flag, so no compare
+	rcrq $1, (%rdi, %rcx, 8)	# shift the long at post %rcx to the right
 	jmp start
 end:
 	ret
