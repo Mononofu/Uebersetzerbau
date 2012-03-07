7c305b0e9ad16531d766fb2e3d261032d9262b99
Julian Schrittwieser <J.Schrittwieser@gmail.com>
2012-03-07 20:58:40 +0100

    added 2nd assemble

---
+++ asmb/asmb.s
@@ -0,0 +1,17 @@
+	.file	"asmb.c"
+	.text
+	.globl	asmb
+	.type	asmb, @function
+asmb:
+.LFB0:
+	.cfi_startproc
+	mov %rsi, %rcx		# start our counter with length of array
+start:
+	rcrq $1, (%rdi, %rcx, 8)
+	loopz start
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	asmb, .-asmb
+	.ident	"GCC: (Ubuntu/Linaro 4.6.1-9ubuntu3) 4.6.1"
+	.section	.note.GNU-stack,"",@progbits
